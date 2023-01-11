e30975d2b23a46961ea3c958aca56bb359355535
benmatheson <mathesonben@gmail.com>
2023-01-10 15:13:09 -0900

    updated bot

--- alaskacam.R
+++ alaskacam.R
@@ -1,45 +1,38 @@
 print("top of the file")
 
 
-
 install.packages("readr")
 install.packages("dplyr")
 install.packages('httr')
 install.packages("stringr")
-install.packages("twitteR")
+# install.packages("twitteR")
+# devtools::install_github("jrowen/twitteR", ref = "oauth_httr_1_0")
+
 install.packages("jsonlite")
+install.packages("rtweet")
 
 # install.package("ggplot2")
 print("after the installation")
 
-library(httr)
+# library(httr)
 library(jsonlite)
 # library(tibble)
-library(twitteR)
+# library(twitteR)
+library(rtweet)
 library(stringr)
 
 print("afterloading")
-# setwd("projects/gh-pfdbot")
-# getwd()
 
-print("after the libraryesi")
-
-
-
-# setwd("projects/alaskacam")
-
+print("after the library")
 
 
 DOT_KEY <- Sys.getenv("DOT_KEY")
 
-
+# url <- str_glue("https://511.alaska.gov/api/v2/get/cameras?key={DOT_KEY}")
 url <- str_glue("https://511.alaska.gov/api/v2/get/cameras?key={DOT_KEY}")
 
 
 
-# camera_data <- httr::GET(url)
-# camera_content <- content(camera_data)
-# camera_unlisted <- lapply(camera_content, unlist)
 
 
 camera_json <- fromJSON(url)
@@ -62,8 +55,8 @@ random_number <- floor( runif(1)*number_of_cameras)
 camera_to_tweet <- cameras[random_number ,]
 
 download.file(url= camera_to_tweet$Url, "pic.jpg")
-info <- file.info("pic.jpg")
-info$size
+# info <- file.info("pic.jpg")
+# info$size
 
 
 camera_tweet <- str_glue("The view from {camera_to_tweet$Name} taken at {camera_to_tweet$LastUpdated}. 📷 from 511.alaska.gov") 
@@ -73,31 +66,93 @@ camera_tweet
 #############twitter stuff#########################
 
 
+
 CONSUMER_KEY <- Sys.getenv("CONSUMER_KEY")
 CONSUMER_SECRET <- Sys.getenv("CONSUMER_SECRET")
 ACCESS_TOKEN <- Sys.getenv("ACCESS_TOKEN")
 SECRET_KEY <- Sys.getenv("SECRET_KEY")
 
 
-c_key_length <- nchar(CONSUMER_KEY)
-c_s_key_length <- nchar(CONSUMER_SECRET)
-a_t_key_length <- nchar(ACCESS_TOKEN)
-s_k_key_length <- nchar(SECRET_KEY)
+# c_key_length <- nchar(CONSUMER_KEY)
+# c_s_key_length <- nchar(CONSUMER_SECRET)
+# a_t_key_length <- nchar(ACCESS_TOKEN)
+# s_k_key_length <- nchar(SECRET_KEY)
 
 
-print("now setting up")
+# print("now setting up")
 
 options(httr_oauth_cache=F)
 
-setup_twitter_oauth(consumer_key = CONSUMER_KEY,
-                    consumer_secret = CONSUMER_SECRET,
-                    access_token = ACCESS_TOKEN,
-                    access_secret = SECRET_KEY)
+
+#####################################################################################################################################################################################################
+
+
+bot <- rtweet_bot(api_key = CONSUMER_KEY , api_secret =CONSUMER_SECRET,  access_token =ACCESS_TOKEN, access_secret = SECRET_KEY )
+
+# bot1 <- rtweet_bot()
+auth_as(bot)
+
+
+# bear_auth <- rtweet_app()
+
+
+# auth_as(bear)
+# auth_get()
+
+# auth_sitrep()
+
+
+# status <- auth_sitrep()
+# status
+
+# auth_as(auth=bot)
+
+# post_tweet(status = "test_auth_bot")
+
+# auth_save(bentoken, "custom_token")
+# auth_as("custom_token")
+
+# auth_as("default")
+
+
+# df <- search_tweets("#MiSTerFPGA")
+# rt <- search_tweets("#rstats", n = 1000, include_rts = FALSE)
+
+
+# auth_get()
+# post_tweet(status = "testbot", token = bot)
+
+
+# str(bot)
+# str(bear)
+# ?post_tweet("test1", token = bear)
+
+
+#####################################################################################################################################################################################################
+
+
+
+# 
+# setup_twitter_oauth(consumer_key = CONSUMER_KEY,
+#                     consumer_secret = CONSUMER_SECRET,
+#                     access_token = ACCESS_TOKEN,
+#                     access_secret = SECRET_KEY)
+# 
+# check_twitter_oauth()
 # 1
 
 
+# setup_twitter_oauth(consumer_key = CONSUMER_KEY,
+#                     consumer_secret = CONSUMER_SECRET)
+# 1
+
+
+# ?setup_twitter_oauth
+
+
 print("setup complete")
-tweet(camera_tweet, mediaPath = "pic.jpg", bypassCharLimit=T)
+# tweet(camera_tweet, mediaPath = "pic.jpg", bypassCharLimit=T)
+post_tweet(status = camera_tweet, media= "pic.jpg", token = bot)
 
 
 ######
