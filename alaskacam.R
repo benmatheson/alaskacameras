f3fb3c327203326d64acd1fa8a7da052d0d1b7b0
Ben Matheson <mathesonben@gmail.com>
2025-05-28 14:44:21 -0800

    Update alaskacam.R
    with vector

--- alaskacam.R
+++ alaskacam.R
@@ -111,7 +111,9 @@ bs_post(
   text= paste0(camera_tweet),
   
   # images = image_blob,
-  images = "pic.jpeg",
+  # images = "pic.jpeg",
+    images = c("pic.jpeg"),
+
   
   images_alt = "a picture from alaska's 511 cameras located on highway"
   
